ef483db6c5ef1095dd20e09192c98ec11f511c9f
thawdezin <thawdezin@gmail.com>
2024-05-08 14:03:23 +0630

    Initial commit

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,22 @@
+import UIKit
+import Flutter
+import workmanager
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        // In AppDelegate.application method
+        //WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "task-identifier")
+        
+        // Register a periodic task in iOS 13+
+       // WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "be.tramckrijte.workmanagerExample.iOSBackgroundAppRefresh", frequency: NSNumber(value: 20 * 60))
+
+        UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
+
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+}
